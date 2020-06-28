952eb758c1f643421739a58ef52dff3dafa6d9d1
rafaelitajahy <rafaelitajahy@gmail.com>
2020-06-28 17:58:49 -0300

    Finalizado

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -39,7 +39,11 @@ const ModalEditFood: React.FC<IModalProps> = ({
 
   const handleSubmit = useCallback(
     async (data: IEditFoodData) => {
-      // EDIT A FOOD PLATE AND CLOSE THE MODAL
+      const { name, price, image, description } = data;
+
+      handleUpdateFood({ name, price, image, description });
+
+      setIsOpen();
     },
     [handleUpdateFood, setIsOpen],
   );
